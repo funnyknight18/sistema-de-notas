7743ad4f04ba9d43a2d70cedca254790b6834c28
Joao Vitor Mocambite Lima <86741407+funnyknight18@users.noreply.github.com>
2022-07-17 14:03:58 -0400

    Create codePageTwo.js

---
+++ codePageTwo.js
@@ -0,0 +1,10 @@
+(function(){
+    'use strict'; 
+  
+const $button_requeste = document.querySelector('[data-js="button-revisa"]'); 
+      
+$button_requeste.addEventListener('click' , function() {
+console.log('deu certo'); 
+} , false );   
+
+})(); 
