a46faa1ef13fb94b60ac0ca4370d5116be2790ae
Joao Vitor Mocambite Lima <86741407+funnyknight18@users.noreply.github.com>
2022-07-18 10:52:04 -0400

    Update codePageTwo.js

--- codePageTwo.js
+++ codePageTwo.js
@@ -2,9 +2,19 @@
     'use strict'; 
   
 const $button_requeste = document.querySelector('[data-js="button-revisa"]'); 
-      
-$button_requeste.addEventListener('click' , function() {
-console.log('deu certo'); 
-} , false );   
+const $portugues = document.querySelector('[data-js="portugues"]');   
+const $ciencia = document.querySelector('[data-js="Ciencias"]');
+const $matematica = document.querySelector('[data-js="Matematica"]');  
+
+
+$button_requeste.addEventListener( 'click' , calculatorNote , false );
+
+function calculatorNote (event) { 
+    event.preventDefault(); 
+   var sum = Number($portugues.value) + Number($matematica.value) + Number($ciencia.value) / 3 ; 
+   $portugues.value >= 6 ? console.log('aprovado em portugues') : console.log('recuperacao em portgues'); 
+   $ciencia.value >= 6 ? console.log('aprovado em ciencia') : console.log('recuperaçao em ciencias'); 
+   $matematica.value >= 6 ? console.log('aprovado em matematica') : console.log('recuperaçao em matematica'); 
+} 
 
 })(); 
